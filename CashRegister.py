ce5f64b97d5065dee8158466ce3c6937678aaaf9
Annas03 <83984612+Annas03@users.noreply.github.com>
2021-07-15 09:04:36 +0500

    Update CashRegister.py

--- CashRegister.py
+++ CashRegister.py
@@ -1,84 +1,5 @@
 from tkinter import *
-
-def checkCashRegister(price, cash, cid=[["ONE", 1000], ["TWO", 2000], ["FIVE", 5000], ["TEN", 10000], ["TWENTY", 20000], ["FIFTY", 500000], ["ONE HUNDRED", 1000000], ["FIVE HUNDRED", 500000], ["ONE THOUSAND", 1000000], ["FIVE THOUSAND", 5000000]]):
-    status = getStatus(price, cash, cid)
-    change = getChange(price, cash, cid, status)
-    return {
-        "status": status,
-        "change": change
-    }
-
-
-def getStatus(price, cash, cid):
-    cashInDrawerArray = cid
-    cinda = []
-    cashInDrawer = 0
-    for item in cashInDrawerArray:
-        cinda.append(item[1])
-        cashInDrawer += item[1]
-    ac = [5000, 1000, 500, 100, 50, 20, 10, 5, 2, 1]
-    status = ""
-    totalChange = float(cash) - float(price)
-    if totalChange > cashInDrawer:
-        status = "INSUFFICIENT_FUNDS"
-    elif totalChange == cashInDrawer:
-        status = "CLOSED"
-    else:
-        status = "OPEN"
-        i = 0
-        while i < len(cinda):
-            if totalChange >= ac[i] and cinda[i] >= ac[i]:
-                totalChange -= ac[i]
-                cinda[i] -= ac[i]
-                totalChange = float(format(totalChange, ".2f"))
-                cinda[i] = float(format(cinda[i], ".2f"))
-            else: 
-                i += 1
-      
-        if totalChange != 0:
-            status = "INSUFFICIENT_FUNDS"
-
-    return status
-
-
-def getChange(price, cash, cid, status):
-    totalChange = float(cash) - float(price)
-    changeArray = []
-    currentCurrencyName = ""
-    currentCurrency = 0
-    change = []
-    ac = [
-    ["FIVE THOUSAND", 5000],
-    ["ONE THOUSAND", 1000],
-    ["FIVE HUNDRED", 500],
-    ["ONE HUNDRED", 100],
-    ["FIFTY", 50],
-    ["TWENTY", 20],
-    ["TEN", 10],
-    ["FIVE", 5],
-    ["TWO", 2],
-    ["ONE", 1]
-    ]
-    cashInDrawerArray = cid
-    i = 0
-    j = 0
-    while i < len(ac):
-        if totalChange >= ac[i][1] and cashInDrawerArray[i][1] >= ac[i][1]:
-            totalChange -= ac[i][1]
-            cashInDrawerArray[i][1] -= ac[i][1]
-            totalChange = float(format(totalChange, ".2f"))
-            cashInDrawerArray[i][1] = float(format(cashInDrawerArray[i][1], ".2f"))
-            j += 1
-        else:
-            if j != 0:
-                currentCurrencyName = ac[i][0]
-                currentCurrency = ac[i][1] * j
-                if status != "INSUFFICIENT_FUNDS":
-                    change = [currentCurrencyName, currentCurrency]
-                    changeArray.append(change)
-            j = 0
-            i += 1
-    return changeArray
+from functions import checkCashRegister
 
 cid = [
 ["FIVE THOUSAND"],
@@ -95,15 +16,15 @@ cid = [
 
 root = Tk()
 root.geometry("800x600")
-img = PhotoImage(file="Template.png")      
-
+img = PhotoImage(file="D:\Annas Docs\Coding\Python\Template.png")      
 
 # Adding Image in BackGround
+
 my_label1 = Label(root, image = img)
 my_label1.place(x = 0, y = 0)
 
-
 # Adding on Heading
+
 my_text = Label(root, text = "Cash Register", fg = "red", font = ("Helvetica", 30))
 my_text.pack()
 
@@ -143,11 +64,23 @@ e2.pack()
 e2.place(x = 250, y = 500)
 e2.insert(0,"")
 
+labels = [
+"Cash in Five Thousand Rupees:- ",
+"Cash in One Thousand Rupees:-",
+"Cash in Five Hundred Rupees:-",
+"Cash in One Hundred Rupees:- ",
+"Cash in Fifty Rupees:- ",
+"Cash in Twenty Rupees:- ",
+"Cash in Ten Rupees:- ",
+"Cash in Five Rupees:- ",
+"Cash in Two Rupees:- ",
+"Cash in One Rupees:- "
+]
 def Clickme2():
     if e2.get() == 'Y' or e2.get() == 'y':
-        for item in cid:
+        for item in cid:    
             x = int(input("Cash in " + item[0] + " Rupee/s:- "))
-            item.append(x)
+            item.append(x)    
         result = checkCashRegister(price, cash1, cid)
         mylabel = Label(root, text = result)
         mylabel.pack()
@@ -163,4 +96,14 @@ mybutton2 = Button(root, text="Confirm", bg="purple", fg="white", command=Clickm
 mybutton2.pack()
 mybutton2.place(x = 500, y = 500)
 
+def Clickme3():
+    return root.quit()
+
+# Quit Button
+
+mybutton3 = Button(root, text="Quit", bg="purple", fg="white", command=Clickme3)
+mybutton3.pack()
+mybutton3.place(x = 600, y = 500)
+
 root.mainloop()
+
