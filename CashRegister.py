10774487e9d2e092905e7d24c4e56d1e1e9809fd
Annas03 <83984612+Annas03@users.noreply.github.com>
2021-07-20 07:32:54 +0500

    Update CashRegister.py

--- CashRegister.py
+++ CashRegister.py
@@ -15,59 +15,54 @@ cid = [
 ]
 
 root = Tk()
-root.geometry("800x600")
-img = PhotoImage(file="Template.png")      
+root.geometry("1920x1080")
+root.title("Cash Register")
 
-# Adding Image in BackGround
+# Due Amount: 
 
-my_label1 = Label(root, image = img)
-my_label1.place(x = 0, y = 0)
+Amount_due_entry = Entry(root, width = 50)
+Amount_due_entry.pack()
+Amount_due_entry.place(x = 25, y = 70)
+Amount_due_entry.insert(0, "")
 
-# Adding on Heading
-
-my_text = Label(root, text = "Cash Register", fg = "red", font = ("Helvetica", 30))
-my_text.pack()
-
-e = Entry(root, width = 50)
-e.pack()
-e.place(x = 250, y = 70)
-e.insert(0,"")
-
-def Clickme():
+def Amount_due():
     global price
-    price = e.get()
+    price = Amount_due_entry.get()
 
-mybutton = Button(root, text="Enter Amount Due", bg="purple", fg="white", command=Clickme)
-mybutton.pack()
-mybutton.place(x = 500, y = 70)
+Amount_due_Button = Button(root, text="Enter Amount Due", bg="purple", fg="white", command=Amount_due)
+Amount_due_Button.pack()
+Amount_due_Button.place(x = 225, y = 70)
 
-e1 = Entry(root, width = 50)
-e1.pack()
-e1.place(x = 250, y = 100)
-e1.insert(0,"")
+# Given Cash:
 
-def Clickme1():
+Cash_given_entry = Entry(root, width = 50)
+Cash_given_entry.pack()
+Cash_given_entry.place(x = 25, y = 100)
+Cash_given_entry.insert(0,"")
+
+def Cash_given():
     global cash1
-    cash1 = e1.get()
+    cash1 = Cash_given_entry.get()
 
-mybutton1 = Button(root, text="Enter Cash Given", bg="purple", fg="white", command = Clickme1)
-mybutton1.pack()
-mybutton1.place(x = 500, y = 100)
+Cash_given_button = Button(root, text="Enter Cash Given", bg="purple", fg="white", command = Cash_given)
+Cash_given_button.pack()
+Cash_given_button.place(x = 225, y = 100)
+
+# Confirming to Update the drawer:
 
 confirmation_Label = Label(root, text = "Do you want to update Cash in Drawer?\n If Yes, press (Y/y) else press any key", padx = 50, bg = "white")
 confirmation_Label.pack()
-confirmation_Label.place(x = 250, y = 450)
+confirmation_Label.place(x = 25, y = 125)
 
-
-e2 = Entry(root, width = 50)
-e2.pack()
-e2.place(x = 250, y = 500)
-e2.insert(0,"")
+confirmation_Entry = Entry(root, width = 10)
+confirmation_Entry.pack()
+confirmation_Entry.place(x = 45, y = 165)
+confirmation_Entry.insert(0,"")
 
 labels = [
 "Cash in Five Thousand Rupees:- ",
-"Cash in One Thousand Rupees:-",
-"Cash in Five Hundred Rupees:-",
+"Cash in One Thousand Rupees:- ",
+"Cash in Five Hundred Rupees:- ",
 "Cash in One Hundred Rupees:- ",
 "Cash in Fifty Rupees:- ",
 "Cash in Twenty Rupees:- ",
@@ -76,34 +71,57 @@ labels = [
 "Cash in Two Rupees:- ",
 "Cash in One Rupees:- "
 ]
-def Clickme2():
-    if e2.get() == 'Y' or e2.get() == 'y':
-        for item in cid:    
-            x = int(input("Cash in " + item[0] + " Rupee/s:- "))
-            item.append(x)    
+data = []
+
+var = IntVar()
+def Submit(data, cid):
+    i = 0
+    for items in cid:
+        j = data[i]
+        if(j.get()==0):
+            items.append(0)
+        else:
+            items.append(int(j.get()))
+        i+=1
+
+def Confirm():
+    if confirmation_Entry.get() == 'Y' or confirmation_Entry.get() == 'y':
+        n = 200
+        for i in range(10):
+            a = Label(root, padx = 20, pady = 5, text=labels[i])
+            a.pack()
+            a.place(x = 0, y = n)
+            n+=25
+            
+        j = 200
+        for entries in range(len(cid)):
+            entry = Entry(root)
+            entry.pack()
+            entry.place(x = 190, y = j)
+            data.append(entry)
+            j+=25
+
+        submit_button = Button(root, padx = 30, text = "Submit", fg = "White", bg = "Purple", command = lambda: [Submit(data, cid), var.set(1)])
+        submit_button.pack()
+        submit_button.place(x = 75, y = 600)
+
+        submit_button.wait_variable(var)
+
         result = checkCashRegister(price, cash1, cid)
-        mylabel = Label(root, text = result)
-        mylabel.pack()
-        mylabel.place(x = 260, y = 550)
+  
     else:
         result = checkCashRegister(price, cash1)
-        mylabel1 = Label(root, text = result)
-        mylabel1.pack()
-        mylabel1.place(x = 260, y = 550)
+       
+    mylabel = Label(root, text = result["status"])
+    mylabel.pack()
+    mylabel.place(x = 260, y = 550)
 
+    mylabel = Label(root, text = result["change"])
+    mylabel.pack()
+    mylabel.place(x = 260, y = 570)
 
-mybutton2 = Button(root, text="Confirm", bg="purple", fg="white", command=Clickme2)
-mybutton2.pack()
-mybutton2.place(x = 500, y = 500)
-
-def Clickme3():
-    return root.quit()
-
-# Quit Button
-
-mybutton3 = Button(root, text="Quit", bg="purple", fg="white", command=Clickme3)
-mybutton3.pack()
-mybutton3.place(x = 600, y = 500)
+confirmation_button = Button(root, text="Confirm", bg="purple", fg="white", command=Confirm)
+confirmation_button.pack()
+confirmation_button.place(x = 126, y = 165)
 
 root.mainloop()
-
